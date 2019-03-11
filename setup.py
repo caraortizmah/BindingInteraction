2c5d1a1b42152bcb5df81d6693e92638ba4bfe86
caraortizmah <caraortizmah@gmail.com>
2019-03-11 10:54:27 -0500

    Update setup.py
    adding things

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import io
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -37,36 +37,26 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Scientific researcher',
         'Topic :: Software Development :: Pipelines :: Python Modules',
         'Topic :: Computational Chemistry',
-
-        # Pick your license as you wish (should match "license" above)
+        'Topic :: Scientific/Engineering :: Chemistry',
         'License :: GNU General Public License v3.0',
-
         'Operating System :: OS Independent',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Bash :: 4.4',
+        'Programming Language :: Bash :: 4.3',
+        'Programming Language :: Tcl :: 8.6',
     ],
 
     # What does your project relate to?
-    keywords='',
+    keywords='Ligand-Receptor Interaction',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
