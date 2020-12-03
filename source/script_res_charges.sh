81d26137161f950179f6c1a51ae1030570a745a0
caraortizmah <caraortizmah@gmail.com>
2020-12-03 15:40:18 -0500

    Create script_res_charges.sh
    formatting residues to be charged

---
+++ source/script_res_charges.sh
@@ -0,0 +1,14 @@
+  #!/bin/bash
+
+#Author: Carlos Andres Ortiz Mahecha
+#caraortizmah@gmail.com
+#This program creates the format of the residues to be charged
+
+cd tobe_charged/
+
+awk '$1=="ATOM"{if ($4=="ASP" || $4=="GLU" || $4=="ARG" || $4=="LYS" || $4=="HIS") {printf "%s %3d %s\n", $4, $6, $5 }}' res_charges.pdb | sort | uniq | awk 'BEGIN{i=1} {printf "%2d. %s\n", i++, $0}' > residues_charges
+
+#using uniq -c, there is a additional column with the numer of repetition of the printed sentence
+#the second awk is only for assinging number at the begining of each row in res_charges
+
+cd ..
