ce5894817dd904c7efaa343296a254acae5608fa
caraortizmah <caraortizmah@gmail.com>
2020-12-03 14:58:38 -0500

    Update addH.sh
    changes in lecture of variables

--- source/addH.sh
+++ source/addH.sh
@@ -7,16 +7,23 @@
 
 mopac16="/opt/mopac/MOPAC2016.exe" #Path of mopac program
 
-for i in `ls *.pdb` #for each pdb files, program execute the lines below
-do
-  
-  j="$(echo "$i" | cut -d'.' -f1)" #remove the extension (".pdb") of the file
-  printf " PDB ADD-H DEBUG AUX PDBOUT\n\n\n" > "$j".mop #Put mopac commands in a .mop file to add hydrogens
+arg="$1"
 
-  awk 'NF=="12"{print $0}' "$i" >> "$j".mop #adding all atoms of the pdb file to the new mopac input
+if [ -z "$arg" ] #execution of this program waits two additional arguments
+then
+  echo "It lacks one argument to execute the script"                                                  
+  echo "For instance: ./addH.sh molecule.pdb" 
+  exit 0
+else
+  echo "Executing script_clean_O.sh over "$arg #This argument is an output pdb file from dowser execution
+fi
 
-  mv "$i" "$i"-old #Remove auxiliar files
-  $mopac16 "$j".mop # Execute input with mopac16
+j="$(echo "$arg" | cut -d'.' -f1)" #remove the extension (".pdb") of the file
+printf " PDB ADD-H GEO-OK DEBUG AUX PDBOUT\n\n\n" > "$j"_H.mop #Put mopac commands in a .mop file to add hydrogens
 
-  echo "fin"
-done
+awk 'NF=="12"{print $0}' "$arg" >> "$j"_H.mop #adding all atoms of the pdb file to the new mopac input
+
+#mv "$arg" "$arg"-old #Remove auxiliar files
+$mopac16 "$j"_H.mop # Execute input with mopac16
+
+echo "fin"
