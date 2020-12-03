6b13d5eebf85c819cd46ab8827b8dfaafa2c91e5
caraortizmah <caraortizmah@gmail.com>
2020-12-03 15:49:50 -0500

    Update script_arc_auxmop.sh
    Fixing old errors

--- source/script_arc_auxmop.sh
+++ source/script_arc_auxmop.sh
@@ -9,7 +9,7 @@ arg="$1" # .arc file
 res="$2" # mutation
 arg2="$3" #option
 
-if !([ -z "$arg" ] && [ -z "$res" ]) && [ -z "$arg2"] # confirming the existence of the three arguments
+if  ! [[ -z "$arg" && -z "$res" ]] && [ -z "$arg2" ] # confirming the existence of the three arguments
 then
   echo "It lacks one argument to execute the script"                       
   echo "For instance: ./script_arc_auxmop.sh file.arc A11 neutral" # an example about of the execution
@@ -18,9 +18,8 @@ else
   echo "Executing script_arc_auxmop.sh over "$arg "and mutation" $res " with " $arg2 " option" 
 fi
 
-#printf " PBD PM7 LET MOZYME GEO_REF=\"SELF\" GNORM=20 GEO-OK THREADS=4 DEBUG PBDOUT AUX\n\n\n" > input_Optall.mop
+printf "   PM6-D3H4X LET 1SCF MOZYME EPS=78.4 GNORM=20 PDBOUT HTML CUTOFF=6\n\n\n" > Opt_"$res"_"$arg2".mop
 
-awk 'BEGIN{i=1} NF=="12"{if ($5=="A" || $5=="B" || $5=="P" || $5=="V") printf "  %6s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,$2,substr($0,16,6),$4,$5,$6,$7,$9,$11}' "$arg" >> Opt_"$res"_"$arg2".mop
-# rewriting the arc file into a .mop file specifing the name of the file with the second and third argument given in the execution
+awk 'BEGIN{i=1} NF=="12"{if ($5=="A" || $5=="B" || $5=="P") printf "  %6s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,$2,substr($0,16,6),$4,$5,$6,$7,$9,$11}' "$arg" >> Opt_"$res"_"$arg2".mop
 
 echo "fin"
