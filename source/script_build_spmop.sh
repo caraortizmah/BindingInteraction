554b8f023c11d4a01b1b9fb966a8a003639fedd3
caraortizmah <caraortizmah@gmail.com>
2020-12-03 16:00:21 -0500

    Update script_build_spmop.sh
    PM6-D3H4X instead of PM7

--- source/script_build_spmop.sh
+++ source/script_build_spmop.sh
@@ -16,11 +16,12 @@ else
   echo "Executing script_editing_inputs.sh over "$arg " to get " $arg".mop version"
 fi
 
-printf " PM7 LET 1SCF MOZYME NSPA=122 EPS=78.4 GNORM=20 GEO-OK THREADS=4 DEBUG PBDOUT AUX\n\n\n" > "$arg".mop # flags for calculate single point 
+printf " PM6-D3H4X LET 1SCF MOZYME EPS=78.4 GNORM=20 PDBOUT CUTOFF=6 HTML\n\n\n" > "$arg".mop
 
 #awk 'BEGIN{i=1} NF=="9"{if ($5=="A" || $5=="B" || $5=="P" || $5=="V") printf "  %s%7d%-2s%5s %1s%5s%13.8f%13.8f%13.8f\n",$1,$2,substr($0,16,6),$4,$5,$6,$7,$8,$9}' "$arg" >> "$arg".mop
 #awk 'BEGIN{i=1} NF=="9" || $5=="A" || $5=="B" || $5=="P" || $5=="V" {if (substr($0,1,2)=="  ")  {printf "  %s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,$2,substr($0,16,6),$4,$5,$6,$7,$8,$9} else{ printf "  %s\n",$0 }}' "$arg" >> "$arg".mop
 
-awk 'BEGIN{i=1} ( NF=="9" ) && ( $5=="A" || $5=="B" || $5=="P" || $5=="V" ) {if (substr($0,1,2)=="  ")  {printf "  %s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,i++,substr($0,16,6),$4,$5,$6,$7,$8,$9} else { printf "  %s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,i++,substr($0,14,6),$4,$5,$6,$7,$8,$9}}' "$arg" >> "$arg".mop # putting format into the .mop file
+# putting format into the .mop file
+awk 'BEGIN{i=1} ( NF=="9" ) && ( $5=="A" || $5=="B" || $5=="P" ) {if (substr($0,1,2)=="  ")  {printf "  %s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,i++,substr($0,16,6),$4,$5,$6,$7,$8,$9} else { printf "  %s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,i++,substr($0,14,6),$4,$5,$6,$7,$8,$9}}' "$arg" >> "$arg".mop 
 
 echo "fin"
