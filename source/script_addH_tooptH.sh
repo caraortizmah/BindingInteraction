09a68c328a145bf5bbaf1a65ad94e01031ed32f2
caom_home <caraortizmah@gmail.com>
2020-12-10 20:38:48 -0500

    fixing error in mopac flag

--- source/script_addH_tooptH.sh
+++ source/script_addH_tooptH.sh
@@ -18,7 +18,7 @@ else
   echo "Executing script_addH_tooptH.sh over "$arg #This argument is an output pdb file from dowser execution
 fi
 
-printf " PM7 LET NOOPT OPT-H MOZYME GEO_OK NSPA=122 EPS=78.4 GNORM=20 PDBOUT HTML\n\n\n" > input_OptH.mop #Put mopac commands in a .mop file to optimize all hydrogens with implicit solvent COSMO
+printf " PM7 LET NOOPT OPT-H MOZYME GEO-OK NSPA=122 EPS=78.4 GNORM=20 PDBOUT HTML\n\n\n" > input_OptH.mop #Put mopac commands in a .mop file to optimize all hydrogens with implicit solvent COSMO
 
 
 awk 'BEGIN{i=1} NF=="12"{printf "  %6s%7d%-2s%3s %1s%5s%13.8f%13.8f%13.8f\n",$1,$2,substr($0,16,6),$4,$5,$6,$7,$9,$11}' "$arg" >> input_OptH.mop #adding all atoms of the "$arg" file to the new mopac input
